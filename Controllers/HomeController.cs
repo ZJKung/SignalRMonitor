2ca639aa74c56401e9b566e5e785d87ad4fb8e55
Ryan <zjkung1123@gmail.com>
2021-04-01 10:54:28 +0800

    add use cpu , ram api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace SignalRMonitor.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<byte[]> MemoryMonster = new List<byte[]>();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -27,6 +28,32 @@ namespace SignalRMonitor.Controllers
         {
             return View();
         }
+
+        [HttpPost("/cpu")]
+        public async Task<IActionResult> UseCPU()
+        {
+            Console.WriteLine("c");
+            var timeout = DateTime.Now.AddSeconds(5);
+            while (DateTime.Now.CompareTo(timeout) < 0)
+            {
+                var newGuid = Guid.NewGuid();
+            }
+            await Task.CompletedTask;
+            return Ok();
+        }
+        [HttpPost("/ram")]
+        public async Task<IActionResult> UseRAM()
+        {
+            byte[] buff = new byte[1024 * 1024 * 1024];
+            for (var i = 0; i < buff.Length; i++) buff[i] = (byte)(i % 256);
+            MemoryMonster.Add(buff);
+            //Sleep 5 seconds to keep the memory space "active"
+            await Task.Delay(5000);
+            //release memory
+            MemoryMonster = new List<byte[]>();
+            return Ok();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
